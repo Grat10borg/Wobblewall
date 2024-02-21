942f1deea0ded3d7eaa3a00881ebc96a8a1a0687
Grat <109081987+Grat10borg@users.noreply.github.com>
2024-02-21 11:46:52 +0100

    changed displayer to use YTiframe API

--- js/displayer.js
+++ js/displayer.js
@@ -11,6 +11,37 @@ let disp = {
 };
 
 function play(link) {
+	let trimedLink = trimLink(link);
+	if(trimedLink == "invalid link")
+		ComfyJS.Say("Link invalid, please give a youtube link!");
+	else {
+	
+	  let div_player = $$.make("div");
+	  div_player.id="player";
+	  disp.displayer.append(div_player);
+			
+      var player;
+	  player = new YT.Player('player', {
+          height: '95%',
+          width: '95%',
+          videoId: 'FTOqCuhQQhM',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+
+		console.log(player)
+		// toggle display animation when ready
+		disp.toggle();
+	}
+}
+
+
+function oldplay(link) {
 		// should filter and then play corisponding function handler
 		let trimedLink = trimLink(link);
 		if(trimedLink == "invalid link")
@@ -37,7 +68,6 @@ function play(link) {
 		// place a youtube player on the displayer
 		
 
-		// toggle display animation when ready
 		disp.toggle();	
 
 		}		
@@ -79,3 +109,38 @@ function toggle() {
 	}
 }
 
+// youtube iframe functions 
+function onYouTubeIframeAPIReady() {
+   player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+	event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+	if (event.data == YT.PlayerState.PLAYING && !done) {
+		setTimeout(stopVideo, 6000);
+		done = true;
+	}
+}
+
+function stopVideo() {
+	player.stopVideo();
+}
