37ab298d448c329929b098f50382e4937357274b
Grat <109081987+Grat10borg@users.noreply.github.com>
2024-02-29 22:18:09 +0100

    added onscreen clock

--- js/taskbar.js
+++ js/taskbar.js
@@ -1,6 +1,16 @@
 "use strict";
 
 let task = {
-	elem: $$.id("chat"),
-	
+	elem: $$.id("taskbar"),
+	elem_time: $$.id("time"),
+	elem_music: $$.id("music"),
+}
+
+/* code for onscreen clock  */
+if(task.elem_time != undefined) {
+	setInterval(() => {
+		let date = new Date().toLocaleTimeString();
+		task.elem_time.innerHTML = date.substring(0, 5)
+		+" "+date.substring(9); 
+	}, 1000)
 }
