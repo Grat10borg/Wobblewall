b237ea0b9d866ff71192bbe5388b9a7bce5a3345
Grat <109081987+Grat10borg@users.noreply.github.com>
2024-03-04 12:32:23 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
     // Chat needs both Channel:manage:broadcast to do stream markers 
     //and clips:edit to clip to your channel with the !clip command
-    MY_API_TOKEN : "o7qjw7f1gmyjppyfrleztbxjokyrdn", 
-    TWITCH_LOGIN : "grat_grot10_berg", // the channel it connects to
-    YOUROAUTH: "o7qjw7f1gmyjppyfrleztbxjokyrdn",
-    BOTOAUTH : "oauth:ittskpnutx42o6zlmps13yotc9zylg",
-    BOTLOGIN : "illu_illusion"
+    MY_API_TOKEN : "<you api token>", 
+    TWITCH_LOGIN : " <your username in your url>", // the channel it connects to
+    YOUROAUTH: "<your Oauth>",
+    BOTOAUTH : "<bot Oauth>",
+    BOTLOGIN : "<bot username>"
 }
 
 var meow = {
